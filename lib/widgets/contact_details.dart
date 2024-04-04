c599c9a720f621984e6fc66cb7b806d4a12f935f
Odufuwa Adebola <adeboladuf@gmail.com>
2024-04-04 15:54:36 +0100

    Made the design of the contacts list better.

--- lib/widgets/contact_details.dart
+++ lib/widgets/contact_details.dart
@@ -9,24 +9,128 @@ class ContactDetails extends StatelessWidget {
 
   final Contact? contact;
 
+  final _nonTransparentContainerheight = 180.0;
+  final _circleAvatarRadius = 50.0;
+  final _transparentAndNonTransparentWidth = 235.0;
+
   @override
   Widget build(BuildContext context) {
+    final transparentContainerHeight =
+        _circleAvatarRadius + _nonTransparentContainerheight;
+
+    Widget activeContent = const Text(
+      'Select a contact from the list on the left',
+      textAlign: TextAlign.center,
+    );
+    if (contact != null) {
+      activeContent = Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: transparentContainerHeight,
+                width: _transparentAndNonTransparentWidth,
+                color: Colors.transparent,
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: _nonTransparentContainerheight,
+                  width: _transparentAndNonTransparentWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: _circleAvatarRadius,
+                      ),
+                      Text(
+                        contact!.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text('Mobile'),
+                          const SizedBox(
+                            width: 7,
+                          ),
+                          Text(
+                            contact!.phoneNumber,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue[500],
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.message,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (_transparentAndNonTransparentWidth / 2) -
+                    _circleAvatarRadius,
+                child: CircleAvatar(
+                  radius: _circleAvatarRadius,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  child: const Icon(Icons.camera_alt),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('History'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Storage locations'),
+          )
+        ],
+      );
+    }
+
     return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
       decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey[400],
+        borderRadius: BorderRadius.circular(15),
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Text(
-              contact == null
-                  ? 'Select a contact from the list on the left'
-                  : contact!.name,
-              textAlign: TextAlign.center,
-            ),
+            child: activeContent,
           ),
         ],
       ),
--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -13,39 +13,79 @@ class ContactsList extends StatelessWidget {
   final void Function(Contact selectedContact) onContactSelected;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        Contact contactN = contactsList[index];
-        return ListTile(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          leading: CircleAvatar(
-            child: Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.red,
-                    Colors.yellow,
-                  ],
-                ),
-              ),
-              child: Text(
-                contactN.name[0],
-                style: const TextStyle(color: Colors.white, fontSize: 25),
+    return Column(
+      children: [
+        const Text(
+          'Phone',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        Text('${contacts.length} contacts with phone number'),
+        const SizedBox(
+          height: 70,
+        ),
+        Row(
+          children: [
+            const Spacer(),
+            const SizedBox(width: 10),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+            const SizedBox(width: 10),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+            const SizedBox(width: 10),
+            Badge(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert),
               ),
             ),
+            const SizedBox(width: 10),
+          ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              Contact contactN = contactsList[index];
+              return ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                leading: CircleAvatar(
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.deepPurple,
+                          Colors.blue,
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      contactN.name[0],
+                      style: const TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ),
+                ),
+                title: Text(contactN.name),
+                onTap: () {
+                  onContactSelected(contactN);
+                },
+              );
+            },
+            itemCount: contacts.length,
           ),
-          title: Text(contactN.name),
-          onTap: () {
-            onContactSelected(contactN);
-          },
-        );
-      },
-      itemCount: contacts.length,
+        ),
+      ],
     );
   }
 }
